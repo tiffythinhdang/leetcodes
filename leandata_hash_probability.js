e3f059541d81f3339ef5e98cfb275de182017646
Thinh Dang <tiffythinhdang@gmail.com>
2019-11-13 10:01:49 -0800

    Add leandata hashMap problem

---
+++ leandata_hash_probability.js
@@ -0,0 +1,29 @@
+const randomKey = (hash) => {
+  const keys = Object.keys(hash);
+  const values = Object.values(hash);
+  const sumValues = values.reduce((a, b) => a + b);
+
+  let randomNumber = Math.floor( Math.random() * sumValues );
+
+  let count = 0;
+  const keyMapToProbability = {};
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    let value = hash[key];
+    keyMapToProbability[key] = [count, count + value - 1];
+    console.log(keyMapToProbability)
+    if (randomNumber >= count && randomNumber <= (count + value - 1)) return key;
+    count += value;
+  }
+} 
+
+let hash = { "a": 1, "b": 2, "c": 5, "d": 2 }
+console.log( randomKey(hash) )
+console.log("---------")
+console.log( randomKey(hash) )
+console.log("---------")
+console.log( randomKey(hash) )
+console.log("---------")
+console.log( randomKey(hash) )
+console.log("---------")
+console.log( randomKey(hash) )
